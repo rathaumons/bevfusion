c53d68664139fa9f04ef7aeef1992fde13119537
rathaROG <10178493+rathaROG@users.noreply.github.com>
2026-03-04 06:02:50 +0100

    Update tools/benchmark.py
    Removed unused import of 'dirty_patches'.

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -2,8 +2,6 @@ import argparse
 import time
 import torch
 
-import dirty_patches
-
 from mmcv import Config
 from mmcv.parallel import MMDataParallel
 from mmcv.runner import load_checkpoint, wrap_fp16_model
