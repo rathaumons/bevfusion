fed8266ba608464f906d2f5b9b78c2988ab1b9fd
rathaumons <ratha.siv@student.umons.ac.be>
2026-03-05 16:40:29 +0100

    Add NOTICE and insert license headers

--- mmdet3d/ops/spconv/conv.py
+++ mmdet3d/ops/spconv/conv.py
@@ -1,3 +1,5 @@
+# Modified by UMONS-Numediart, Ratha SIV in 2026.
+
 # Copyright 2019 Yan Yan
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import math
 import numpy as np
 import torch
